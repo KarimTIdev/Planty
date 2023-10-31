230bc445b23aad223424d3f54aeba74a74a957ad
KarimTIdev <k.tiouriidrissi@gmail.com>
2023-10-31 12:57:53 +0100

    Projet terminé à 100%

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -1,10 +1,11 @@
 <?php
 
+add_filter( 'wpcf7_autop_or_not', '__return_false');
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() 
 {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() , '/styles.css');
-	wp_enqueue_style(' theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+	wp_enqueue_style( 'theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
 
 // Hook Filter pour l'affichage du lien "Admin"
@@ -17,7 +18,7 @@ function add_admin_link( $items, $args )
 			$admin_link = '<li><a href="' . get_admin_url() . '">Admin</a></li>';
 			$menu_items = explode('</li>', $items);
 			array_splice($menu_items, 1, 0, $admin_link);
-			$items = implode('</li>', $menu_items);
+			$items = implode('', $menu_items);
 		}
 			return $items;
 	}
